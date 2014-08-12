766af62de0accc6982debb376748951f227f25d1
tanay1337 <tanay1337@gmail.com>
2014-08-12 11:09:05 +0530

    Fixed the Copyright Character

--- extras/footer.php
+++ extras/footer.php
@@ -2,7 +2,7 @@
 <body>
 <div id="footer" style="opacity: 0.8; position:absolute; left:0px; bottom:0px; height:30px; width:100%; background-color: #000000;">
 <div id="footer_left" style="color: #ffffff; float: center; width: 70%; margin-left: 10%; font-family: 'Arimo', sans-serif;">
-			<font size="2px">Copyright © 2014 Tanay Pant. All rights reserved.</font>
+			<font size="2px">Copyright (c) 2014 Tanay Pant. All rights reserved.</font>
 		</div>
 </div>
 </body>
